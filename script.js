302a36afa16516bf55d8804caf391ec734d60d56
nagaraju Vuppala <vpp.nagaraju1234@gmail.com>
2026-03-24 10:57:24 +0530

    students order

--- script.js
+++ script.js
@@ -1104,6 +1104,11 @@ async function initStudents() {
       html += '<div class="stu-group-title">Ongoing PhD Students</div>';
       html += twoColWrap(current);
     }
+
+    if (alumni.length) {
+      html += '<div class="stu-group-title">PhD Students Supervised</div>';
+      html += twoColWrap(alumni);
+    }
 	if( dual_degree.length) {
 	  html += '<div class="stu-group-title">MS/Dual Degree Students</div>';
 	  html += twoColWrap(dual_degree);
@@ -1112,10 +1117,6 @@ async function initStudents() {
 	//   html += '<div class="stu-group-title">MS Students</div>';
 	//   html += twoColWrap(ms);
 	// }
-    if (alumni.length) {
-      html += '<div class="stu-group-title">Alumni</div>';
-      html += twoColWrap(alumni);
-    }
 
     c.innerHTML = html;
     attachStudentListeners(c);
